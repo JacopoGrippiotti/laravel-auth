c9f68813c9debcc7fad22d94550f380599f59152
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-24 19:53:07 +0200

    added image to Projects table through migration

--- database/migrations/2023_08_22_164551_create_projects_table.php
+++ database/migrations/2023_08_22_164551_create_projects_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('content');
             $table->string('slug')->default('');
             $table->text('url');
+            $table->text('image');
             $table->timestamps();
         });
     }
